463384ca981bffedb5c679b8ed9c069a1aa84548
just-boris <just-boris@yandex-team.ru>
2014-03-18 19:47:32 +0400

    use true through

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 var crypto = require('crypto');
 var path = require("path");
-var through = require('through');
+var through = require('through2');
 
 module.exports = function(options) {
     options = options || {append: true};
-    return through(process);
+    return through.obj(process);
 
     function getOutputPath(filepath, checksum) {
         return [
@@ -17,12 +17,13 @@ module.exports = function(options) {
         ].join('');
     }
 
-    function process(file) {
+    function process(file, enc, callback) {
         if (file.isBuffer()) {
             var content = file.contents.toString('utf-8'),
                 checksum = crypto.createHash('md5').update(content, "utf8").digest("hex");
             file.path = getOutputPath(file.path, checksum);
         }
-        this.emit('data', file);
+        this.push(file);
+        callback();
     }
 };
